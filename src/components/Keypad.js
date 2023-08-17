e53e53c4c4efdb3ac16d38cfa4f511f08ba35338
MARY NJOKI <marynjoki738@gmail.com>
2023-08-17 07:42:07 +0300

    event handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const handleInputChange = () => {
+        console.log('Entering password...');
+      };
+    
     return (
-        <div></div>
+        <div>
+            <input type="password" placeholder ="Enter password" onChange={handleInputChange}/>
+        </div>
     )
 }
 
